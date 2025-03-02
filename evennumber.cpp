4449a3415fe6775844f0d1f6b70557df75a1b8d1
Sonali Tiwari <sonalitiwari1249@gmail.com>
2025-03-02 23:02:25 +0530

    Add files via upload

---
+++ evennumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"even number"<<endl;
+	}
+	else{
+		cout<<"odd number"<<endl;
+	
+	}
+
+	return 0;
+}
+
+
